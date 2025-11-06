6437f9d3620cf3924a8e291ad8b796dacb2b8c00
m_debbarh <mdebbarh04@gmail.com>
2025-11-07 00:22:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,348 @@
+document.addEventListener('DOMContentLoaded',()=> {
+let cards=[
+{
+    id: 0,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 1,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 2,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 3,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 4,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 5,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 6,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 7,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 8,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+},
+{
+    id: 9,
+    name: "the first darklord",
+    illustration: "/images/first darklord.jpg",
+    type: "fairy/fudion/effect",
+    rarity: "UR",
+    level: 12,
+    attribute: "dark",
+    atk_def: "ATK/ 4000   DEF/ 4000",
+    materials: "3 DARK Fairy monsters",
+    description: [
+                 "Your opponent cannot target Fairy monsters you control with card effects.",
+                 "You can only use 1 of the following effects of 'The First Darklord' per turn, and only once that turn.",
+                 "● If this card is Fusion Summoned using 'Darklord Morningstar' as material: You can destroy all cards your opponent controls.",
+                 "● During the Main Phase (Quick Effect): You can pay 1000 LP; Special Summon 1 Fairy monster from your hand or GY in Defense Position."
+                 ],
+    quantity: 50,
+    cardType: "fusion",
+}
+
+];
+
+
+    const cardsContainer= document.querySelector(".card-grid");
+    cards.forEach((card, index)=>{
+        if(card.quantity)
+        {
+            const cardLayer= document.createElement('div');
+            cardLayer.classList.add('shadow-2xl','rounded-xl', 'w-full');
+            const RarityLayer= document.createElement('div');
+            RarityLayer.classList.add('flex', 'justify-end');
+            const Rarity= document.createElement('p');
+            Rarity.classList.add('bg-gradient-to-r', 'from-purple-500', 'to-blue-500', 'text-white', 'font-bold', 'text-center', 'text-sm', 'p-1', 'rounded-t-xl', 'w-1/4');
+            Rarity.textContent = card.rarity ;
+            RarityLayer.append(Rarity);
+
+
+            const blackLayer= document.createElement('div');
+            blackLayer.classList.add('bg-stone-950', 'p-1.5' , 'rounded-b-xl');
+            const colorLayer= document.createElement('div');
+            colorLayer.classList.add( 'p-3', 'flex', 'flex-col', 'items-center', 'shadow-xl', 'rounded-lg');
+            if(card.cardType=="monser")
+            {
+                colorLayer.classList.add('bg-orange-400')
+            
+            } else if(card.cardType=="monser effect")
+            {
+                colorLayer.classList.add('bg-orange-500')
+            } else if(card.cardType=="fusion")
+            {
+                colorLayer.classList.add('bg-purple-500')
+
+            } else if(card.cardType=="link")
+            {
+                colorLayer.classList.add('bg-blue-500')
+        
+            } else if(card.cardType=="trap")
+            {
+                colorLayer.classList.add('bg-pink-500')
+
+            } else if(card.cardType=="spell")
+            {
+                colorLayer.classList.add('bg-emerald-300')
+            }
+            
+            
+            const nameContainer= document.createElement('div');
+            nameContainer.classList.add('w-full', 'mb-2', 'flex', 'justify-between', 'items-center', 'text-white');
+            const name= document.createElement('p');
+            name.classList.add('text-base', 'sm:text-lg', 'font-extrabold', 'truncate');
+            name.textContent= card.name;
+            const attribute= document.createElement('span');
+            
+            attribute.classList.add('bg-purple-700', 'text-xs', 'font-semibold', 'px-2', 'py-0.5', 'rounded-full', 'shadow-md');
+            attribute.textContent= card.attribute;
+            nameContainer.append(name);
+            nameContainer.append(attribute);
+        
+        
+            const levelContainer= document.createElement('div');
+            levelContainer.className="stars-container w-full space-x-0.5 mb-2";
+            for(let i=0; i<card.level; i++)
+            {
+                const star= document.createElement('span');
+                star.className= "bg-orange-500 text-yellow-500 text-xs px-1 py-0.5 rounded-full shadow-lg";
+                star.textContent="★";
+                levelContainer.append(star);
+            }
+
+
+            const imageContainer= document.createElement('div');
+            imageContainer.className="w-full aspect-square bg-gray-900 rounded-md shadow-inner overflow-hidden mb-3";
+            const image= document.createElement('img');
+            image.src= card.illustration;
+            image.alt= card.name;
+            image.className="w-full h-full object-cover";
+            imageContainer.append(image);
+            
+            
+            const detailsContainer= document.createElement('div');
+            detailsContainer.className= "w-full p-2 border-2 border-gray-800 rounded-md shadow-2xl text-gray-800";
+            if(card.cardType=="monser")
+            {
+                detailsContainer.classList.add('bg-orange-300')
+            
+            } else if(card.cardType=="monser effect")
+            {
+                detailsContainer.classList.add('bg-orange-300')
+            } else if(card.cardType=="fusion")
+            {
+                detailsContainer.classList.add('bg-purple-300')
+
+            } else if(card.cardType=="link")
+            {
+                detailsContainer.classList.add('bg-blue-300')
+        
+            } else if(card.cardType=="trap")
+            {
+                detailsContainer.classList.add('bg-pink-300')
+
+            } else if(card.cardType=="spell")
+            {
+                detailsContainer.classList.add('bg-emerald-200')
+            }
+
+            const typeConatiner= document.createElement('div');
+            typeConatiner.className= "text-xs sm:text-sm font-semibold mb-1";
+            const types= document.createElement('p');
+            types.textContent= card.type;
+            typeConatiner.append(types);
+            
+            
+            
+            const description = document.createElement('div');
+            description.className= "text-[0.65rem] sm:text-xs leading-snug";
+            card.description.forEach((parargraph)=>{
+                const para= document.createElement('p');
+                para.className="mb-1";
+                para.textContent= parargraph;
+                description.append(para);
+            });
+
+
+            const atk_def= document.createElement('p');
+            atk_def.className="w-full text-right font-bold text-xs sm:text-sm mt-2";
+            atk_def.textContent= card.atk_def;
+            detailsContainer.append(typeConatiner);
+            
+            if(card.materials != '')
+            {
+                const materialConatiner= document.createElement('div');
+                materialConatiner.className="text-xs sm:text-sm mb-1";
+                const materials= document.createElement('p');
+                materials.className= "italic";
+                materials.textContent= card.materials;
+                materialConatiner.append(materials);
+                detailsContainer.append(materialConatiner)
+            }
+
+            detailsContainer.append(description);
+            detailsContainer.append(atk_def);
+        
+        
+            colorLayer.append(nameContainer);
+            colorLayer.append(levelContainer);
+            colorLayer.append(imageContainer);
+            colorLayer.append(detailsContainer);
+            blackLayer.append(colorLayer);
+            cardLayer.append(RarityLayer);
+            cardLayer.append(blackLayer);
+            cardsContainer.append(cardLayer);
+        }
+    
+    });
+})
